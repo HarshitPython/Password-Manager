0ea1270a6d4aabd8624431f8e8d8ee9f2068adf0
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-07 21:39:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def generate_password():
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
-#     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
 #     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
 #     password_list = password_letters + password_symbols + password_numbers
