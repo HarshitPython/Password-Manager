effa0bb501a5d9faedcc6852020ffcb786e3731e
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-07 21:39:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def generate_password():
 
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-#     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
 #     password_list = password_letters + password_symbols + password_numbers
 #     shuffle(password_list)
