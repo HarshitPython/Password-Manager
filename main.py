c2cf7c8b92305a838bb4e8e767ff22c8979ae809
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-09 12:02:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def save():
         try:
             with open("data.json", "r") as data_file:
                 #Reading old data
-#                 data = json.load(data_file)
+                data = json.load(data_file)
 #         except FileNotFoundError:
 #             with open("data.json", "w") as data_file:
 #                 json.dump(new_data, data_file, indent=4)
