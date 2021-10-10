4fa866a1b3d336e110f00d90a95c3034c203fdd7
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-10 12:56:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def find_password():
     except FileNotFoundError:
         messagebox.showinfo(title="Error", message="No Data File Found.")
     else:
-#         if website in data:
+        if website in data:
 #             email = data[website]["email"]
 #             password = data[website]["password"]
 #             messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
