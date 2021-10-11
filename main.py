b9ca5f4be9d490231a68b5ee711f46338ff5c656
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-11 12:45:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-# canvas = Canvas(height=200, width=200)
+canvas = Canvas(height=200, width=200)
 # logo_img = PhotoImage(file="logo.png")
 # canvas.create_image(100, 100, image=logo_img)
 # canvas.grid(row=0, column=1)
