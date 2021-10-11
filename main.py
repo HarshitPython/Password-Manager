4c3b662df894c5aa1961e3c63a9bba3af61cc511
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-11 12:46:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-# canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=1)
 
 # #Labels
 # website_label = Label(text="Website:")
