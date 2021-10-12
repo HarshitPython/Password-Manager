f4e2797ad27260982d157e7c55e13ad350795b45
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-12 10:16:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-# #Entries
+#Entries
 # website_entry = Entry(width=21)
 # website_entry.grid(row=1, column=1)
 # website_entry.focus()
