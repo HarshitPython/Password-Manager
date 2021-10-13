4d81b7961d701d8f167d9f9d31090afefeef82f5
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-13 10:44:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ search_button = Button(text="Search", width=13, command=find_password)
 search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
-# add_button = Button(text="Add", width=36, command=save)
+add_button = Button(text="Add", width=36, command=save)
 # add_button.grid(row=4, column=1, columnspan=2)
 
 # window.mainloop()
